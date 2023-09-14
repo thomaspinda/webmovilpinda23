cf02fd1635d02376799f4f5313df0cdd2b8421aa
Thomas Pinda <thomaspinda23@gmail.com>
2023-09-14 12:02:16 -0300

    pinda

--- JS/hola.js
+++ JS/hola.js
@@ -1,1 +1,8 @@
 alert("Hola Mundo");
+console.log("Hola");
+console.log("Mundo");
+console.log("Numeros");
+[1,2,3,4].forEach(console.log);
+console.log("2"/2);
+/*let edad = prompt("¿Cuál es tu edad?",0);
+alert('tienes '+ edad +' años');*/
